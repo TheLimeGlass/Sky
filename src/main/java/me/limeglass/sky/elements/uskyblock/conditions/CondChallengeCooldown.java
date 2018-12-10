04f1bbd0f36ddeeed5e243f8122aff7ddcfad6d2
TheLimeGlass <seantgrover@gmail.com>
2018-12-10 05:04:27 -0700

    First commit

--- src/main/java/me/limeglass/sky/elements/uskyblock/conditions/CondChallengeCooldown.java
+++ src/main/java/me/limeglass/sky/elements/uskyblock/conditions/CondChallengeCooldown.java
@@ -1,12 +1,14 @@
 package me.limeglass.sky.elements.uskyblock.conditions;
 
 import ch.njol.skript.conditions.base.PropertyCondition;
+import me.limeglass.sky.Sky;
 import me.limeglass.sky.interfaces.challenges.SkyblockChallenge;
 
 public class CondChallengeCooldown extends PropertyCondition<SkyblockChallenge> {
 
 	static {
-		register(CondChallengeCooldown.class, PropertyType.HAVE, "[a] cooldown", "challenges");
+		if (!Sky.getSkyblock().isASkyBlock())
+			register(CondChallengeCooldown.class, PropertyType.HAVE, "[a] cooldown", "challenges");
 	}
 	
 	@Override
